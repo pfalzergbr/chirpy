7870471c973c2b13aa78a82ef75ecfb5292f57c7
Gabor Pfalzer <pfalzer@gmail.com>
2023-06-11 07:24:32 +0100

    add a file server to serve an asset

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+
+func middleware(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS"{
+			w.WriteHeader(http.StatusOK)
+		}
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main(){
-	fmt.Println("Chirp!")
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+
+	corsMux := middleware(mux)
+
+	
+	log.Fatal(http.ListenAndServe(":8080", corsMux))
+
 }
