9f74081563e15487b73a8d9775dee512c6328a66
Gabor Pfalzer <pfalzer@gmail.com>
2023-06-13 07:28:59 +0100

    add simple hit count middleware

--- main.go
+++ main.go
@@ -5,19 +5,23 @@ import (
 	"net/http"
 )
 
+type ApiConfig struct {
+	fileServerHits int
+}
 
-// func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-// 	w.WriteHeader(http.StatusOK)
-// 	w.Write([]byte("OK"))
-// }
+func (cfg *ApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileServerHits++
+	return next
+}
 
 func main() {
 	const filepathRoot = "./"
 	const port = "8080"
 
+	cfg := &ApiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc((http.StripPrefix("/app", http.FileServer(http.Dir("."))))))
 	mux.HandleFunc("/healthz", handlerReadiness)
 
 	corsMux := middleware(mux)
@@ -33,7 +37,6 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 
 }
-
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
