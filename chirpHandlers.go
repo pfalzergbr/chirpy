8e38d365171b31ddb6880df7d2b76174213209bf
Gabor Pfalzer <pfalzer@gmail.com>
2023-06-15 07:52:53 +0100

    add profanity filter

--- chirpHandlers.go
+++ chirpHandlers.go
@@ -3,14 +3,21 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+var profanities = []string{
+	"kerfuffle",
+	"sharbert",
+	"fornax",
+}
 
 func handleChirpsValidate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,7 +34,22 @@ func handleChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	cleanedBody := cleanProfanities(params.Body)
+
 	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: cleanedBody,
 	})
+}
+
+func cleanProfanities(body string) string {
+	bodyWords := strings.Split(body, " ")
+	for wIdx, w := range bodyWords {
+		for _, p := range profanities {
+			if strings.EqualFold(w, p) {
+				bodyWords[wIdx] = "****"
+			}
+		}
+	}
+
+	return strings.Join(bodyWords, " ")
 }
