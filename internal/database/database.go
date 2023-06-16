0ab70f62ab6bd4d56de223edf16ba828975a078c
Gabor Pfalzer <pfalzer@gmail.com>
2023-06-16 07:41:01 +0100

    add createChirp

--- internal/database/database.go
+++ internal/database/database.go
@@ -50,6 +50,43 @@ func (db *DB) ensureDB() error {
 	return nil
 }
 
+func (db *DB) GetChirps() ([]Chirp, error){
+	dbStruct, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+
+	chirps := make([]Chirp, 0, len(dbStruct.Chirps))
+
+	for _, chirp := range dbStruct.Chirps {
+		chirps = append(chirps, chirp)
+	}
+
+	return chirps, nil
+}
+
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+	dbStruct, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	id := len(dbStruct.Chirps) + 1
+	chirp := Chirp{
+		Id:   id,
+		Body: body,
+	}
+
+	dbStruct.Chirps[id] = chirp
+
+	err = db.writeDB(dbStruct)
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	return chirp, nil
+}
+
 func (db *DB) loadDB() (DBStructure, error) {
 	db.mux.RLock()
 	defer db.mux.RUnlock()
